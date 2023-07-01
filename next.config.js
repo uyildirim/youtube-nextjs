46189c65e56834df726c4781afc8ba8baf8da0d2
uyildirim <uguryildirim1903@gmail.com>
2023-07-01 15:44:32 +0300

    Uptade

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'images.pexels.com',
           },
+          {
+            protocol: 'https',
+            hostname: 'loremflickr.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'picsum.photos',
+          },
+          {
+            protocol: 'https',
+            hostname: 'cloudflare-ipfs.com',
+          },
         ],
       },
 }
