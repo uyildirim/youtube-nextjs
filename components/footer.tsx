91bf9659dc0831d028cbbdd383121de99e20642d
uyildirim <uguryildirim1903@gmail.com>
2023-07-17 20:33:36 +0300

    Footer

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-between p-7">
+      <span className="font-bold">Admin Dashboard</span>
+      <span className="text-sm">YILDIRIM Admin Dashboard</span>
+    </div>
+  );
+}
